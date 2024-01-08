913a67f7f11899e8f7b6fd4a0e1507078a1e18a6
Leonardo Batini <batinileo@gmail.com>
2024-01-07 23:21:56 -0300

    new exercises

---
+++ beginner/bee1005.js
@@ -0,0 +1,11 @@
+var A = 5.0
+var B = 7.1
+var pesoA = 3.5
+var pesoB = 7.5
+
+var A = A * pesoA
+var B = B * pesoB
+
+const media = (A + B) / (pesoA + pesoB)
+
+console.log('MEDIA =', media.toFixed(5))
---
+++ beginner/bee1006.js
@@ -0,0 +1,14 @@
+var A = 5.0
+var B = 6.0
+var C = 7.0
+var pesoA = 2
+var pesoB = 3
+var pesoC = 5
+
+var A = A * pesoA
+var B = B * pesoB
+var C = C * pesoC
+
+const media = (A + B + C) / (pesoA + pesoB + pesoC)
+
+console.log('MEDIA =', media)
